c56c8764b4a28072509ce4055479d46dac6c6d35
Ohimai Matthew <matthew4ohimai15@gmail.com>
2024-07-03 07:28:11 -0700

    My First Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Creation of file in /tmp with puppet
+
+file {'BeginnersDrill':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
