225bb719226e6e7ce54469dfceac5bf9ea87ec32
Ohimai Matthew <matthew4ohimai15@gmail.com>
2024-07-03 08:15:13 -0700

    Installation Of Flask With Puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,16 @@
 #Installation Of Flask Package Using Puppet
 
-package {'Python 3.8':
+package { 'python3.8':
   ensure   => '3.8.10',
   provider => 'pip3',
 }
 
-package {'Flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-package {'Werkzeug':
+package { 'Werkzeug':
   ensure   => '2.1.1'
   provider => 'pip3',
   require  => Package['Flask'],
