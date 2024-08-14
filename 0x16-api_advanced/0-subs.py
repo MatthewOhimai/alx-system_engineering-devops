6e5e9a65dd9a6fd87024adda1b39a76fc3fb5ce0
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-08-14 08:19:52 +0100

    API Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,24 @@
-#!/usr/bin/python3
-"""
-Script that queries the subscribers on a given Reddit subreddit
-"""
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-     if response.status_code == 200:
-         data = response.json()
-         subscribers = data['data']['subscribers']
-         return subscribers
-     else:
-         return 0
+    # Define the Reddit API URL for the subreddit's about page
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    # Define custom headers with a User-Agent to avoid Too Many Requests errors
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    try:
+        # Make the GET request to the Reddit API
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        # Check if the response status code is 200 (OK)
+        if response.status_code == 200:
+            data = response.json()
+            # Return the number of subscribers
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            # If the subreddit is invalid, return 0
+            return 0
+    except requests.RequestException:
+        # If there is an exception in the request, return 0
+        return 0
